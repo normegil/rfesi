44ac78c01ca04deb292c488524d3f9527639480f
Olivier Ory <olivier.ory@uliege.be>
2023-11-22 11:50:33 +0100

    Skills Endpoint - TRained skills

--- src/groups/skills.rs
+++ src/groups/skills.rs
@@ -6,3 +6,31 @@ use crate::prelude::*;
 pub struct SkillsGroup<'a> {
     pub(crate) esi: &'a Esi,
 }
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[allow(missing_docs)]
+pub struct CharacterSkills {
+    pub skills: Vec<Skill>,
+    pub total_sp: i64,
+    pub unallocated_sp: Option<i32>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[allow(missing_docs)]
+pub struct Skill {
+    pub active_skill_level: i32,
+    pub skill_id: i32,
+    pub skillpoints_in_skill: i64,
+    pub trained_skill_level: i32,
+}
+
+impl<'a> SkillsGroup<'a> {
+    api_get!(
+        /// List all trained skills for the given character
+        get_character_skills,
+        "get_characters_character_id_skills",
+        RequestType::Authenticated,
+        CharacterSkills,
+        (character_id: i32) => "{character_id}"
+    );
+}
